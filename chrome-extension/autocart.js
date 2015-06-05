6eff8d127f0a9e9c30e17fd145aa406bcb7e473a
Blaine Gunn <bgunn@alliancehealth.com>
2015-06-05 16:26:47 -0600

    init commit

---
+++ chrome-extension/autocart.js
@@ -0,0 +1,24 @@
+var size = "9";
+var amount = 1;
+
+function addToCart() {
+	var sizesList=document.getElementsByName("skuAndSize")[0];
+	function setQuantity() {
+		document.getElementsByName("qty")[0].selectedIndex = amount-1;
+	}
+	function setSizeValue() {
+		for (var i=0; i<sizesList.length; i++){
+			if(sizesList.options[i].text == size) {
+				document.getElementsByName("skuAndSize")[0].selectedIndex = i;
+				setQuantity();
+			}
+		}
+	}
+	if(sizesList != undefined) {
+	    setSizeValue();
+	    document.getElementsByClassName("js-add-to-cart add-to-cart nsg-button nsg-grad--nike-orange")[0].click();
+	} else {
+	    setTimeout("addToCart()", 250);
+	}
+}
+setTimeout("addToCart()", 250);
